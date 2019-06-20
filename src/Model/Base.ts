4248002a0994e091b312a64512ddd7d0e00deaf6
Steven <stevennhnk113@gmail.com>
2019-06-20 15:56:05 -0700

    Added postcontroller

---
+++ src/Model/Base.ts
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+export class BaseModel {
+	public id: string = null;
+	public CreatedAt: firebase.firestore.Timestamp;
+	public UpdatedAt: firebase.firestore.Timestamp;
+
+	public MakedForDelete: boolean = false;
+
+	public Update(obj: BaseModel) {
+		this.id = obj.id;
+		this.CreatedAt = obj.CreatedAt;
+		this.UpdatedAt = firebase.firestore.Timestamp.fromDate(new Date());
+		this.MakedForDelete = obj.MakedForDelete;
+	}
+}
