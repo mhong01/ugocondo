31b305e02bf05994c74bc853f5e964783442d1a7
Minh Hong <hellyeyoudo@gmail.com>
2019-08-01 14:47:17 -0700

    contratc

--- src/View/ContractItem.tsx
+++ src/View/ContractItem.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { PostModel } from "../Model/Post";
 import { Card, CardActionArea, CardMedia, CardContent, Typography, CardActions, Button } from "@material-ui/core";
-import { Link as RouterLink } from 'react-router-dom';
+import { Link as RouterLink, Redirect } from 'react-router-dom';
 import { ContactStatusEnum } from "../Model/Contract";
 import ContractAndPost from "../Model/ContractAndPost";
 import ContractControllerInstance from "../Controller/ContractController";
+import UserControllerInstance from "../Controller/UserController";
+
 
 class ContractItem extends React.Component <any, any, any>{
 
@@ -19,9 +21,19 @@ class ContractItem extends React.Component <any, any, any>{
             console.log("Props null");
             return;
         } 
+
+        this.state = {
+            to: null,
+			redirectToReferrer: !UserControllerInstance._IsSignedIn,
+        }
         this.post = props.Post;
         console.log(this.post);
         this.setButtons = this.setButtons.bind(this);
+        this.onApproveSelected = this.onApproveSelected.bind(this);
+        this.onCancelSelected = this.onCancelSelected.bind(this);
+        this.onDiscardSelected = this.onDiscardSelected.bind(this);
+        this.onDeleteCancelSelected = this.onDeleteCancelSelected.bind(this);
+        this.onDeleteSelected = this.onDeleteSelected.bind(this);
     }
 
 
@@ -30,21 +42,38 @@ class ContractItem extends React.Component <any, any, any>{
             if (this.post.Status == ContactStatusEnum.Pending){
                 return (
                     <div>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        {"Somebody wants to rent this place!"}
+                    </Typography>
                     <Button size="small" color="primary" onClick={()=> this.onApproveSelected()}>
                         Approve
                     </Button>
-                    <Button size="small" color="primary">
-                    Discard
-                    </Button></div>
+                    <Button size="small" color="primary" onClick={()=> this.onDiscardSelected()}>
+                        Discard
+                    </Button>
+                    <Button size="medium" color="secondary" onClick={()=> this.onDeleteSelected()}>
+                        Delete Request
+                    </Button> </div>
                 );
             } else if (this.post.Status == ContactStatusEnum.Rented){
-                return (<Button size="small" color="primary">
-                        Cancel
-                        </Button>);
+                return (<div>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                            {"This request is already approved! Click to cancel Approval"}
+                        </Typography>
+                        <Button size="small" color="primary" onClick={()=> this.onDiscardSelected()}>
+                            Cancel Approval
+                        </Button> </div>);
             } else {
-                return (<Button size="small" color="primary">
-                        Delete
-                        </Button>);
+                return (<div>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        {"This request is already discarded! Click Cancel to reopen!"}
+                     </Typography>
+                    <Button size="small" color="primary" onClick={()=> this.onDeleteCancelSelected()}>
+                        Cancel
+                    </Button>
+                    <Button size="medium" color="secondary" onClick={()=> this.onDeleteSelected()}>
+                        Delete Request
+                    </Button>  </div>);
             }
         }
     }
@@ -57,10 +86,82 @@ class ContractItem extends React.Component <any, any, any>{
         contract.PostID = this.post.Contract.PostID;
         contract.Status = ContactStatusEnum.Rented;
 
-        return await ContractControllerInstance.UpdateContract(contract);
+        let obj = await ContractControllerInstance.UpdateContract(contract);
+        if (obj != null){
+            this.setState({ to: "/request" });
+			this.setState({ redirectToReferrer: true })
+        }
+        this.post.Status = contract.Status;
+        this.forceUpdate();
+    }
+
+    async onDiscardSelected(){
+        console.log("Discard");
+        let contract = this.post.Contract;
+        contract.OnwerID = this.post.Contract.OnwerID;
+        contract.RenterID = this.post.Contract.RenterID;
+        contract.PostID = this.post.Contract.PostID;
+        contract.Status = ContactStatusEnum.Declined;
+
+        let obj = await ContractControllerInstance.UpdateContract(contract);
+        if (obj != null){
+            this.setState({ to: "/request" });
+			this.setState({ redirectToReferrer: true })
+        }
+        this.post.Status = contract.Status;
+        this.forceUpdate();
+    }
+    
+
+    async onCancelSelected(){
+        console.log("Discard");
+        let contract = this.post.Contract;
+        contract.OnwerID = this.post.Contract.OnwerID;
+        contract.RenterID = this.post.Contract.RenterID;
+        contract.PostID = this.post.Contract.PostID;
+        contract.Status = ContactStatusEnum.Declined;
+
+        let obj = await ContractControllerInstance.UpdateContract(contract);
+        if (obj != null){
+            this.setState({ to: "/request" });
+			this.setState({ redirectToReferrer: true })
+        }
+        this.post.Status = contract.Status;
+        this.forceUpdate();
+    }
+
+    async onDeleteCancelSelected(){
+        console.log("Discard");
+        let contract = this.post.Contract;
+        contract.OnwerID = this.post.Contract.OnwerID;
+        contract.RenterID = this.post.Contract.RenterID;
+        contract.PostID = this.post.Contract.PostID;
+        contract.Status = ContactStatusEnum.Pending;
+
+        let obj = await ContractControllerInstance.UpdateContract(contract);
+        if (obj != null){
+            this.setState({ to: "/request" });
+			this.setState({ redirectToReferrer: true })
+        }
+        this.post.Status = contract.Status;
+        this.forceUpdate();
+    }
+    
+    async onDeleteSelected(){
+        console.log("Delete")
+        let contract = this.post.Contract;
+        let obj = await ContractControllerInstance.DeleteContract(contract.id);
+        if (obj != null){
+            this.setState({ to: "/request" });
+			this.setState({ redirectToReferrer: true })
+        }
+        this.forceUpdate();
     }
 
     render(){
+        if (this.state.redirectToReferrer) {
+			return <Redirect to={this.state.to} />;
+		}
         var styles = {
             media: {
                 height: 250,
@@ -91,6 +192,8 @@ class ContractItem extends React.Component <any, any, any>{
                 </CardActionArea>
                 <CardActions>
                     {this.setButtons()}
+
+                    
                 </CardActions>
             </Card>
         );
